b006b631d43740c9309c05e9c9b2997b0e819347
Nartex <garadraw@gmail.com>
2023-03-14 21:40:51 +0100

    Testing React props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+const Hello = (props) =>
+{
+	console.log('Reached the Hello Component')
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	console.log(props)
+	return (
+	  <div>
+		<p>Hello {props.name}, you are {props.age} years old</p>
+
+	  </div>
+	)
+}
+  
+const App = () =>
+{
+	console.log('Reached the App Component')
+
+	// const name = 'Peter'
+	// const age = 10
+
+	return (
+	  <div>
+		<h1>Greetings</h1>
+		<Hello name='George' age={13 + 54} />
+        {/* <Hello name={name} age={age} /> */}
+		
+
+	  </div>
+	)
 }
 
-export default App;
+const footer = () => 
+{
+	return (
+		<div>
+			greeting app created by <a href='https://github.com/garadraw'>garadraw</a>
+		</div>
+	)
+}
+
+// const App = () => 
+// {	
+// 	const now = new Date()
+// 	const a = 10
+// 	const b = 20
+// 	console.log('Reached the App variable')
+// 	console.log(now, a+b)
+	
+// 	return (
+// 	<div>
+// 	  <p>Hello world it is {now.toString()}</p>
+// 	  <p>{a} plus {b} is {a+b}</p>
+// 	  <p>E pur si muove</p>
+// 	</div>
+//   )
+// }
+
+// const App = () => {
+// 	const now = new Date()
+// 	const a = 10
+// 	const b = 20
+// 	return React.createElement(
+// 	  'div',
+// 	  null,
+// 	  React.createElement(
+// 		'p', null, 'Hello world, it is ', now.toString()
+// 	  ),
+// 	  React.createElement(
+// 		'p', null, a, ' plus ', b, ' is ', a + b
+// 	  )
+// 	)
+//   }
+
+export default App
